f01d93083494cde69e634eb8029afe15c4bf95b1
Kunyao Lan <lankunyao@sjtu.edu.cn>
2024-04-07 19:42:02 +0800

    show tex

--- _bibliography/process.py
+++
@@ -1,9 +0,0 @@
-with open("papers.bib", "r") as f:
-    lines = f.readlines()
-    output = []
-    for line in lines:
-        output.append(line)
-        if "@" in line:
-            output.append("  bibtex_show={true},\n")
-    with open("papers_new.bib", "w") as f:
-        f.writelines(output)
